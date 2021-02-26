3fc862bc4e904dda91b85668aa695c6553337826
Bangxin Hu <hu.bangxin@gmail.com>
2021-02-26 17:31:31 -0500

    Add behavior to image component

--- tests/integration/components/rental/image-test.js
+++ tests/integration/components/rental/image-test.js
@@ -26,4 +26,28 @@ module('Integration | Component | rental/image', function(hooks) {
     assert.dom('.image img').hasAttribute('src', '/assets/images/teaching-tomster.png');
     assert.dom('.image img').hasAttribute('alt', 'Teaching Tomster');
   });
+
+   test('clicking on the component toggles its size', async function(assert) {
+    await render(hbs`
+      <Rental::Image
+        src="/assets/teaching-tomster.png"
+        alt="Teaching Tomster"
+      />
+    `);
+
+    assert.dom('button.image').exists();
+
+    assert.dom('.image').doesNotHaveClass('large');
+    assert.dom('.image small').hasText('View Larger');
+
+    await click('button.image');
+
+    assert.dom('.image').hasClass('large')
+    assert.dom('.image small').hasText('View Smaller');
+
+    await click('button.image');
+
+    assert.dom('.image').doesNotHaveClass('large');
+    assert.dom('.image small').hasText('View Larger');
+  });
 });
